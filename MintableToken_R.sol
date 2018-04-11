8237fc117d879a5a1f6d31c22d3cd27fee33ef5c
danskyy <35762103+danskyy@users.noreply.github.com>
2018-04-11 23:03:27 +0300

    Added finishMinting func

--- MintableToken_R.sol
+++ MintableToken_R.sol
@@ -3,9 +3,26 @@ import "./StandartToken_R.sol";
 import "./Ownable.sol";
 
 contract MintableToken_R is StandartToken_R, Ownable {
+    bool public mintingFinished = false;
+    
+    modifier allowedMinting {
+        require((msg.sender == owner) && !mintingFinished);
+        _;
+    }
+    
     function mint(address _to, uint _amount) public onlyOwner {
         assert((totalSupply + _amount >= totalSupply) && (balances[_to] + _amount >= balances[_to]));
         balances[_to] += _amount;
         totalSupply += _amount;
+        Mint(_to, _amount);
     }
+    
+    function finishMinting() allowedMinting public returns(bool) {
+        mintingFinished = true;
+        MintFinished();
+        return true;
+    }
+    
+    event Mint(address indexed _to, uint _amount);
+    event MintFinished();
 }
